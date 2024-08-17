0b36013fa0bdf2d72a25cceeece72ef12c128785
AngelesT85 <brawl-ford123456@mail.ru>
2024-08-17 08:40:54 +0300

    Create images.py

---
+++ images.py
@@ -0,0 +1,13 @@
+from pygame.image import load
+
+OneDeckShipImg = load("images/OneDeckShip.png")
+ShipStartImg = load("images/ShipStart.png")
+ShipContinueImg = load("images/ShipContinue.png")
+ShipEndImg = load("images/ShipEnd.png")
+FieldImg = load("images/Field.png")
+HitImg = load("images/Hit.png")
+SkipImg = load("images/Skip.png")
+RestartImg = load("images/Restart.png")
+YouWinImg = load("images/YouWin.png")
+YouLoseImg = load("images/YouLose.png")
+GameStart = load("images/GameStart.png")
