07a580f56c3323d22005f37c79dea10a679c4458
AngelesT85 <brawl-ford123456@mail.ru>
2024-08-17 08:48:21 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
 import pygame as pg
+
+def game():
+    is_game = True
+    pg.init()
+
+    screen = pg.display.set_mode((1024, 900))
+    screen.fill((255, 255, 255))
+
+    while is_game:
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                is_game = False
+                break
+        
+        pg.display.flip()
+
+game()
